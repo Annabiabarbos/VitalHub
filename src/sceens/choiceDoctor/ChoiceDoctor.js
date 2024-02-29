6cb17a872acc4684c58e934d0fb27c69901d4980
Annabiab_ <annabbarbosa2712@gmail.com>
2024-02-28 21:51:18 -0300

    Página médico finalizada

--- src/sceens/choiceDoctor/ChoiceDoctor.js
+++ src/sceens/choiceDoctor/ChoiceDoctor.js
@@ -1,23 +1,50 @@
 import { ScrollView } from "react-native"
 import { Container } from "../../components/container/Style"
-import { FilterAppointamentDoctor } from "../../components/doctorCard/Style"
 import { Title } from "../../components/title/Style"
-import { DoctorCard, ImageCard, TitleCard } from "./Style"
-import { TextAge, TextBold } from "../../components/appointmentCard/Style"
+import { ButtonChoiceDoctor, ContCard, DoctorCard, FilterAppointamentDoctor, ImageCard, TextArea, TextButtonDoctor, TitleCard } from "./Style"
+import { Button, ButtonSecondary, ButtonSecondaryTitle } from "../../components/button/Style"
+import { ButtonText } from "../../components/appointmentCard/Style"
+import { ButtonTittle } from "../../components/buttonTitle/Style"
 
-export const ChoiceDoctor = () => {
+export const ChoiceDoctor = ({navigation}) => {
     return (
         <Container>
 
             <Title>Selecionar médico</Title>
             <ScrollView>
-                <FilterAppointamentDoctor>
-                    <DoctorCard>
+
+                <DoctorCard>
+                    <ContCard>
                         <ImageCard source={{ uri: "../../../assets/Doctor1.png" }} />
                         <TitleCard>Dra Alessandra</TitleCard>
-                    </DoctorCard>
-                    <DoctorCard />
-                </FilterAppointamentDoctor>
+                    </ContCard>
+                    <TextArea>Dermatologia, Esteticista</TextArea>
+                </DoctorCard>
+                <DoctorCard>
+                    <ContCard>
+                        <ImageCard source={{ uri: "../../../assets/Doctor1.png" }} />
+                        <TitleCard>Dr Kumushiro</TitleCard>
+                    </ContCard>
+                    <TextArea>Cirurgião, Cardiologista</TextArea>
+                </DoctorCard>
+                <DoctorCard>
+                    <ContCard>
+                        <ImageCard source={{ uri: "../../../assets/Doctor1.png" }} />
+                        <TitleCard>Dr Rodrigo Santos</TitleCard>
+                    </ContCard>
+                    <TextArea>Clínico, Pediatra</TextArea>
+                </DoctorCard>
+
+               <ButtonChoiceDoctor>
+               <ButtonTittle>Continuar</ButtonTittle>
+               </ButtonChoiceDoctor>
+
+               <ButtonSecondary onPress={() => navigation.navigate("ChoiceClinic")}>
+                    <ButtonSecondaryTitle>
+                        Cancelar
+                    </ButtonSecondaryTitle>
+                </ButtonSecondary> 
+
             </ScrollView>
 
 
