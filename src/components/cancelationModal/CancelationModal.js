5f71caf243c8218fe1fc50dc53250fe83be5c26a
Anna Beatriz Barbosa Cheliga Horta <52161105850@info.local>
2024-03-18 10:51:42 -0300

    Implementação da notificação

--- src/components/cancelationModal/CancelationModal.js
+++ src/components/cancelationModal/CancelationModal.js
@@ -4,8 +4,49 @@ import { Title } from "../title/Style"
 import { Button, ButtonSecondary, ButtonSecondaryTitle } from "../button/Style"
 import { ButtonTittle } from "../buttonTitle/Style"
 
-export const CancellationModal = ({ visible, setShowModalCancel, ...rest}) => {
-    return(
+import * as notifications from "expo-notifications"
+
+notifications.requestPermissionsAsync();
+
+notifications.setNotificationHandler({
+    handleNotification: async () => ({
+
+        //mostra o alerta quando a notificacao for recebida
+        shouldShowAlert: true,
+
+        //define o som da notificacao
+        shouldPlaySound: true,
+
+        //configura o numeor de notificacao0es no icone do app
+        shouldSetBadge: false,
+    })
+})
+
+export const CancellationModal = ({ visible, setShowModalCancel, ...rest }) => {
+
+    const CancelationNotification = async () => {
+
+        //ststus da permissao
+        const { status } = await notifications.getPermissionsAsync();
+
+        // verifica se o usuario concedeu permissao
+        if (status !== "granted") {
+            console.log("Você não ativou as notificações");
+            return;
+        }
+        //enviar as notifications 
+        await notifications.scheduleNotificationAsync({
+            content: {
+                title: "Consulta cancelada",
+                body: "Sua consulta marcada para o dia 10/04 ás 14:15 foi cancelada",
+            },
+            trigger: null/*{
+            seconds: 5
+          }*/
+        })
+
+    }
+    return (
 
         <Modal {...rest} visible={visible} transparent={true} animationType="fade">
 
@@ -16,7 +57,7 @@ export const CancellationModal = ({ visible, setShowModalCancel, ...rest}) => {
 
                     <ModalText>Ao cancelar essa consulta, abrirá uma possível disponibilidade no seu horário, deseja mesmo cancelar essa consulta?</ModalText>
 
-                    <Button>
+                    <Button onPress={CancelationNotification}>
                         <ButtonTittle>Confirmar</ButtonTittle>
                     </Button>
 
@@ -24,11 +65,11 @@ export const CancellationModal = ({ visible, setShowModalCancel, ...rest}) => {
                         <ButtonSecondaryTitle>
                             Cancelar
                         </ButtonSecondaryTitle>
-                    </ButtonSecondary> 
+                    </ButtonSecondary>
 
                 </ModalContent>
             </PatientModal>
-  
+
         </Modal>
     )
 }
